06c1fbf3c5cce81c52d61773ccb1212a6e411004
Shivam Sharma <prisonmatch@gmail.com>
2020-03-04 16:52:40 +0530

    static filtering

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/controller/FilteringController.java
@@ -0,0 +1,23 @@
+package com.in28minutes.rest.webservices.restfulwebservices.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.in28minutes.rest.webservices.restfulwebservices.model.SomeBean;
+
+@RestController
+public class FilteringController {
+
+	@GetMapping("/filtering")
+	public SomeBean retrieveSomeBean() {
+		return new SomeBean("value1", "value2", "value3");
+	}
+
+	@GetMapping("/filtering-list")
+	public List<SomeBean> retrieveListOfSomeBean() {
+		return Arrays.asList(new SomeBean("value1", "value2", "value3"), new SomeBean("value1", "value2", "value3"));
+	}
+}
