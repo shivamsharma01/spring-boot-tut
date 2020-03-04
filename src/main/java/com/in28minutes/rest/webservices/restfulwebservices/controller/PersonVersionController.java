cd9f553cff642a1b07dff9cb289d8285965d771d
Shivam Sharma <prisonmatch@gmail.com>
2020-03-04 19:31:00 +0530

    versioning restful services

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/controller/PersonVersionController.java
@@ -0,0 +1,57 @@
+package com.in28minutes.rest.webservices.restfulwebservices.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.in28minutes.rest.webservices.restfulwebservices.model.Name;
+import com.in28minutes.rest.webservices.restfulwebservices.model.PersonV1;
+import com.in28minutes.rest.webservices.restfulwebservices.model.PersonV2;
+
+@RestController
+public class PersonVersionController {
+
+	//uri versioning
+	@GetMapping("/v1/person")
+	public PersonV1 personV1() {
+		return new PersonV1("Bob Charlie");
+	}
+
+	@GetMapping("/v2/person")
+	public PersonV2 personV2() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+	
+	//request param versioning
+	@GetMapping(value = "/person/param", params="version=1")
+	public PersonV1 paramV1() {
+		return new PersonV1("Bob Charlie");
+	}
+
+	@GetMapping(value = "/person/param", params="version=2")
+	public PersonV2 paramV2() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+	
+	//header versioning
+	@GetMapping(value = "/person/header", headers="X-API-VERSION=1")
+	public PersonV1 headerV1() {
+		return new PersonV1("Bob Charlie");
+	}
+	
+	@GetMapping(value = "/person/header", headers="X-API-VERSION=2")
+	public PersonV2 HeaderV2() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+	
+	// mediatype versioning
+	@GetMapping(value = "/person/produces", produces="application/vnd.company.app-v1+json")
+	public PersonV1 producesV1() {
+		return new PersonV1("Bob Charlie");
+	}
+	
+	@GetMapping(value = "/person/produces", produces="application/vnd.company.app-v2+json")
+	public PersonV2 producesV2() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+
+}
